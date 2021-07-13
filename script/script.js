699921c283f57e73f758ef04478202ace1f40ff8
dbsrlwns <rjswmr@naver.com>
2021-07-13 13:36:17 +0900

    Livart_Page

---
+++ script/script.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+$(function (){
+    $("#priceBox").click(function (){
+        $("#layerBG").css(
+            {"display" : "block"}
+        );
+    });
+    
+    $("#priceBox2").click(function (){
+        $("#layerBG").css(
+            {"display" : "block"}
+        );
+    });
+    
+    $("#closeBtn").click(function (){
+        $("#layerBG").css(
+            {"display" : "none"}
+        );
+    });
+});
